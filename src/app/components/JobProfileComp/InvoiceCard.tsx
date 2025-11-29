4dd6973eb7f0cd84c0bb0727629e3cc9572113aa
Development <66966682+teamsoftapps@users.noreply.github.com>
2025-11-29 06:03:30 +0500

    Update InvoiceCard.tsx

--- src/app/components/JobProfileComp/InvoiceCard.tsx
+++ src/app/components/JobProfileComp/InvoiceCard.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { SlOptions } from "react-icons/sl";
 import { BsSend } from "react-icons/bs";
+import { useRouter } from 'next/navigation';
 
 const InvoiceCard = () => {
+  const router = useRouter()
   return (
     <div className='h-32 bg-[#F4F4F5] w-full text-black p-3 rounded-2xl inter'>
         <div className='flex items-center justify-between'>
@@ -21,7 +23,7 @@ const InvoiceCard = () => {
 
         <div className='flex items-center justify-between mt-3 '>
             <div className='flex items-center gap-4'>
-                 <button className='cursor-pointer rounded-4xl  p-1 border-[#01B0E9] border-1  flex items-center gap-2 px-3'><BsSend className='w-4 h-4'/><span> View</span></button>
+                 <button onClick={()=>router.push("/addInvoice?id=1")} className='cursor-pointer rounded-4xl  p-1 border-[#01B0E9] border-1  flex items-center gap-2 px-3'><BsSend className='w-4 h-4'/><span> View</span></button>
                   <button className='cursor-pointer rounded-4xl  p-1 border-[#01B0E9] border-1  flex items-center gap-2 px-3'><BsSend className='w-4 h-4'/><span>Send</span> </button>
             </div>
             <button className='cursor-pointer rounded-full  p-2 border-[#01B0E9] border-1 '><SlOptions className='w-4 h-4'/> </button>
